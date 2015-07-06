a99d8a2b36ceae9b093de8c26fa952adb30ca418
DarylPinto <daryl.j.pinto@gmail.com>
2015-07-06 15:06:14 -0400

    Fixed bugs, updated UI, added optional liquipedia link for players

--- js/main.js
+++ js/main.js
@@ -34,6 +34,7 @@ function printFormattedTable(){
 	var P1name = $("#PlayerOneName").val();
 	var P1twitch = $("#PlayerOneTwitch").val();
 	var P1twitter = $("#PlayerOneTwitter").val();
+	var P1liquipedia = $("#PlayerOneLiquipedia").val();
 	var P1media = "";
 
 	var P2 = $("#PlayerTwo").val();
@@ -41,35 +42,34 @@ function printFormattedTable(){
 	var P2name = $("#PlayerTwoName").val();
 	var P2twitch = $("#PlayerTwoTwitch").val();
 	var P2twitter = $("#PlayerTwoTwitter").val();
+	var P2liquipedia = $("#PlayerTwoLiquipedia").val();
 	var P2media = "";
 
-	var setCount = $("#SetCount").val();
-
-	var game1Winner = $("#GameOne .Winner").val();
+	var game1Winner = $("#GameOne .Winner").val().toUpperCase();
 	var game1P1Character = $("#GameOne .PlayerOneCharacter").val();
 	var game1P2Character = $("#GameOne .PlayerTwoCharacter").val();
 	var game1StockCount = parseInt($("#GameOne .StocksRemaining").val());
 	var game1Stage = $("#GameOne .Stage").val();
 
-	var game2Winner = $("#GameTwo .Winner").val();
+	var game2Winner = $("#GameTwo .Winner").val().toUpperCase();
 	var game2P1Character = $("#GameTwo .PlayerOneCharacter").val();
 	var game2P2Character = $("#GameTwo .PlayerTwoCharacter").val();
 	var game2StockCount = parseInt($("#GameTwo .StocksRemaining").val());
 	var game2Stage = $("#GameTwo .Stage").val();
 
-	var game3Winner = $("#GameThree .Winner").val();
+	var game3Winner = $("#GameThree .Winner").val().toUpperCase();
 	var game3P1Character = $("#GameThree .PlayerOneCharacter").val();
 	var game3P2Character = $("#GameThree .PlayerTwoCharacter").val();
 	var game3StockCount = parseInt($("#GameThree .StocksRemaining").val());
 	var game3Stage = $("#GameThree .Stage").val();
 
-	var game4Winner = $("#GameFour .Winner").val();
+	var game4Winner = $("#GameFour .Winner").val().toUpperCase();
 	var game4P1Character = $("#GameFour .PlayerOneCharacter").val();
 	var game4P2Character = $("#GameFour .PlayerTwoCharacter").val();
 	var game4StockCount = parseInt($("#GameFour .StocksRemaining").val());
 	var game4Stage = $("#GameFour .Stage").val();
 
-	var game5Winner = $("#GameFive .Winner").val();
+	var game5Winner = $("#GameFive .Winner").val().toUpperCase();
 	var game5P1Character = $("#GameFive .PlayerOneCharacter").val();
 	var game5P2Character = $("#GameFive .PlayerTwoCharacter").val();
 	var game5StockCount = parseInt($("#GameFive .StocksRemaining").val());
@@ -103,7 +103,7 @@ function printFormattedTable(){
 
 	function generatePlayerMedia(){
 
-		if(P1twitter != "" || P1twitch != ""){
+		if(P1twitter != "" || P1twitch != "" || P1liquipedia){
 			if(P1name === ""){
 				P1name = P1;
 			}
@@ -119,9 +119,15 @@ function printFormattedTable(){
 				}
 				P1media = P1media.concat("[Twitter](https://twitter.com/" + P1twitter + ")");
 			}
+			if(P1liquipedia != ""){
+				if(P1twitch != "" || P1twitter != ""){
+					P1media = P1media.concat(" | ")
+				}
+				P1media = P1media.concat("[Liquipedia](" + P1liquipedia + ")");
+			}
 		}
 
-		if(P2twitter != "" || P2twitch != ""){
+		if(P2twitter != "" || P2twitch != "" || P2liquipedia){
 			if(P2name === ""){
 				P2name = P2;
 			}
@@ -137,6 +143,12 @@ function printFormattedTable(){
 				}
 				P2media = P2media.concat("[Twitter](https://twitter.com/" + P2twitter + ")");
 			}
+			if(P2liquipedia != ""){
+				if(P2twitch != "" || P2twitter != ""){
+					P2media = P2media.concat(" | ")
+				}
+				P2media = P2media.concat("[Liquipedia](" + P2liquipedia + ")");
+			}
 		}
 	}
 
@@ -148,6 +160,22 @@ function printFormattedTable(){
 		}else{
 			printLine("ERROR: WINNER MUST BE 'P1' OR 'P2'")
 		}
+	}
+
+	function getSetCount(){
+		var P1score = 0;
+		var P2score = 0;
+
+		[game1Winner,game2Winner,game3Winner,game4Winner,game5Winner].forEach(function(winner){
+			if(winner === "P1"){
+				P1score += 1;
+			}
+			if(winner === "P2"){
+				P2score += 1;
+			}
+		});
+
+		return P1score.toString() + " - " + P2score.toString();
 	}
 
 	generatePlayerMedia();
@@ -167,19 +195,21 @@ function printFormattedTable(){
 		printLine("")
 	}
 
-	printLine(P1 + " " + P1mains.map(makeFlair).toString().replace(/,/g , " ") + " | " + setCount + " | " + P2 + " " + P2mains.map(makeFlair).toString().replace(/,/g , " "));
+	printLine(P1 + " " + P1mains.map(makeFlair).toString().replace(/,/g , " ") + " | " + getSetCount() + " | " + P2 + " " + P2mains.map(makeFlair).toString().replace(/,/g , " "));
 	printLine("---:|:--:|:---");
 
 	displayRow(game1Winner, game1P1Character, game1P2Character, game1Stage, game1StockCount)
 	displayRow(game2Winner, game2P1Character, game2P2Character, game2Stage, game2StockCount)
-	displayRow(game3Winner, game3P1Character, game3P2Character, game3Stage, game3StockCount)
 	
+	if($("#GameThree .Stage").val() != ""){
+		displayRow(game3Winner, game3P1Character, game3P2Character, game3Stage, game3StockCount)
+	}
 	if($("#GameFour .Stage").val() != ""){
 		displayRow(game4Winner, game4P1Character, game4P2Character, game4Stage, game4StockCount)
 	}
 	if($("#GameFive .Stage").val() != ""){
 		displayRow(game5Winner, game5P1Character, game5P2Character, game5Stage, game5StockCount)
 	}
-
-	printLine("---");
+	printLine("<span style=\"color: white\">*^^Generated ^^by [^^Tournament ^^Tabler](http://darylpinto.github.io)*</span>");
+	printLine("___");
 }
